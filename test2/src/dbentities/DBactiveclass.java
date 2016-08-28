4ae24983a57b29a1fe641a1c9213650b69f9287b
matthias <maurer.matthias@gmx.at>
2016-08-28 16:26:24 +0200

    added active class - serialization

---
+++ test2/src/dbentities/DBactiveclass.java
@@ -0,0 +1,62 @@
+package dbentities;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+@Root(name="class")
+public class DBactiveclass extends DBentity{
+	public int creator;
+	public String data;
+	
+	@Element(name="visibility")
+	public Visibility visibility;
+	@Element(name="name")
+	public String name;
+	@Element(name="description")
+	public String description;
+	@Element(name="id")
+	public int classid;
+	
+	public DBactiveclass(){}
+	
+	/*
+	public DBactiveclass(String name, String description, Visibility visibility, int creatorId){
+		this.name = name;
+		this.description= description;
+		this.visibility = visibility;
+		this.creator = creatorId;
+	}
+	
+	public DBactiveclass(Document doc){
+		if(doc.getElementsByTagName("name").getLength()>0)
+			this.name = doc.getElementsByTagName("name").item(0).getFirstChild().getNodeValue();
+		if(doc.getElementsByTagName("description").getLength()>0)
+			this.description = doc.getElementsByTagName("description").item(0).getFirstChild().getNodeValue();
+		if(doc.getElementsByTagName("visibility").getLength()>0)
+			this.visibility =  (doc.getElementsByTagName("visibility").item(0).getFirstChild().getNodeValue().equals("ALL")) ? Visibility.ALL : Visibility.NOTALL;
+	}
+	
+	public String toXMLWithCstructure(){
+		String xml ="<class>";
+		xml+="<visibility>"+this.visibility+"</visibility>";
+		xml+="<name>"+this.name+"</name>";
+		xml+="<description>"+this.description+"</description>";
+		xml+="<id>"+this.classid+"</id>";
+		DBcompetencestructure cstruct =  DBConnector.getCStructureById(DBConnector.getCstructureIdByClassId(this.classid));
+		if(cstruct != null)
+			xml +="<competencestructure>"+cstruct.name+"</competencestructure>";
+		this.classid=DBConnector.getClassIdByName(this.name);
+		List<DBentity> entities = DBConnector.getClassTaskLinkageByClassId(this.classid);
+		if(!entities.isEmpty()){
+			xml+="<tasks>";
+			for(int i=0;i<entities.size();i++){
+				xml+="<task>"+DBConnector.getTaskById(((DBlinkageclasstask)entities.get(i)).taskid).name+"</task>";
+			}
+			xml+="</tasks>";
+		}
+		xml+="</class>";
+		
+		return xml;
+	}
+	*/
+}
